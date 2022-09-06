660a14b3bd2e9a705b49ef16aa47fde79c14809a
Evan Mitchell <evanm4183@gmail.com>
2022-09-06 14:15:13 -0500

    HumanPlayer done

--- Program.cs
+++ Program.cs
@@ -46,6 +46,13 @@ namespace ShootingDice
 
             Console.WriteLine("-------------------");
 
+            HumanPlayer humanPlayer = new HumanPlayer();
+            humanPlayer.Name = "Brittany";
+
+            humanPlayer.Play(player1);
+
+            Console.WriteLine("-------------------");
+
             List<Player> players = new List<Player>() {
                 player1, player2, player3, large
             };
